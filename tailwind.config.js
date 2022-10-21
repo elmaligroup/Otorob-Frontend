19f2b7e4566e5ab05323de62d6a72064700d3fd8
efnan <efnanozkann@gmail.com>
2022-10-21 18:12:39 +0300

    Main page oluşturuldu.

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
+    backgroundImage: {
+      'slider': "url('../images/slider.svg')",
+      'slider2': "url('../images/slider2.svg')",
+      'footer': "url('../images/footer.png')"
+  },
+    
+    extend: { 
+      colors: {
+      titles: "#020044",
+      price: "#0000A2",
+      ship: "#096301"
+    },
+    fontFamily: { 
+      poppins: "'Poppins', sans-serif",
+   },
+   
+     
+  },
+  plugins: [],
+}
+}
