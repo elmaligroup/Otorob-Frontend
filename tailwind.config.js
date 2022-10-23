ee0263905715276ed0ee4b494649748527fd2b50
efnanozkan <efnanozkann@gmail.com>
2022-10-23 17:48:24 +0300

    registration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./Pages/*", "index.html"],
   theme: {
     screens: {
       'sm': '640px',
